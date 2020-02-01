36cac32ea1eda857a55d47b93abfa6fd16a765fc
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2020-02-01 12:02:26 +0900

    Update first desgin

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -24,4 +24,3 @@ new Vue({
   router,
   store
 }).$mount('#app')
-
--- assets/js/components/Card.vue
+++ assets/js/components/Card.vue
@@ -1,46 +1,94 @@
 <template>
-  <div class="flex max-w-5xl mx-auto justify-between flex-wrap">
-    <div class="bg-yellow-300 text-center mb-4 w-pxcard">
-      <p>カードリスト</p>
-      <input
-        type="number"
-        :value="10"
-      >
+  <div class="p-24 flex flex-wrap items-center justify-center">
+    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-yellow-abs rounded-lg max-w-xs shadow-lg">
+      <div class="relative pt-10 px-10 flex items-center justify-center">
+        <div
+          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
+        />
+        <img
+          class="relative w-40"
+          src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
+          alt=""
+        >
+      </div>
+      <div class="relative text-white px-6 pb-6 mt-6">
+        <div class="flex justify-center">
+          <span class="block bg-white rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$36.00</span>
+        </div>
+      </div>
     </div>
-    <div class="bg-yellow-300 text-center mb-4 w-pxcard">
-      <p>カードリスト</p>
-      <input
-        type="number"
-        :value="10"
-      >
+    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-red-arm rounded-lg max-w-xs shadow-lg">
+      <div class="relative pt-10 px-10 flex items-center justify-center">
+        <div
+          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
+        />
+        <img
+          class="relative w-40"
+          src="https://user-images.githubusercontent.com/2805249/64069998-305de300-cc9a-11e9-8ae7-5a0fe00299f2.png"
+          alt=""
+        >
+      </div>
+      <div class="relative text-white px-6 pb-6 mt-6">
+        <div class="flex justify-center">
+          <span class="block bg-white rounded-full text-teal-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$45.00</span>
+        </div>
+      </div>
     </div>
-    <div class="bg-yellow-300 text-center mb-4 w-pxcard">
-      <p>カードリスト</p>
-      <input
-        type="number"
-        :value="10"
-      >
+    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-purple-foot rounded-lg max-w-xs shadow-lg">
+      <div class="relative pt-10 px-10 flex items-center justify-center">
+        <div
+          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
+        />
+        <img
+          class="relative w-40"
+          src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
+          alt=""
+        >
+      </div>
+      <div class="relative text-white px-6 pb-6 mt-6">
+        <div class="flex justify-center">
+          <span class="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$68.50</span>
+        </div>
+      </div>
     </div>
-    <div class="bg-yellow-300 text-center mb-4 w-pxcard">
-      <p>カードリスト</p>
-      <input
-        type="number"
-        :value="10"
-      >
+    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-green-yoga rounded-lg max-w-xs shadow-lg">
+      <div class="relative pt-10 px-10 flex items-center justify-center">
+        <div
+          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
+        />
+        <img
+          class="relative w-40"
+          src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
+          alt=""
+        >
+      </div>
+      <div class="relative text-white px-6 pb-6 mt-6">
+        <div class="flex justify-center">
+          <span class="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$68.50</span>
+        </div>
+      </div>
     </div>
-    <div class="bg-yellow-300 text-center mb-4 w-pxcard">
-      <p>カードリスト</p>
-      <input
-        type="number"
-        :value="10"
-      >
-    </div>
-    <div class="bg-yellow-300 text-center mb-4 w-pxcard">
-      <p>カードリスト</p>
-      <input
-        type="number"
-        :value="10"
-      >
+    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-pink-heel rounded-lg max-w-xs shadow-lg">
+      <div class="relative pt-10 px-10 flex items-center justify-center">
+        <div
+          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
+        />
+        <img
+          class="relative w-40"
+          src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
+          alt=""
+        >
+      </div>
+      <div class="relative text-white px-6 pb-6 mt-6">
+        <div class="flex justify-center">
+          <span class="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$68.50</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
