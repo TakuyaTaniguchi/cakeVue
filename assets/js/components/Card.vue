39f412f0ae49eecdacf4ab066a688eac080f947d
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2020-02-03 14:36:29 +0900

    Remove img

--- assets/js/components/Card.vue
+++ assets/js/components/Card.vue
@@ -1,92 +1,50 @@
 <template>
-  <div class="p-24 flex flex-wrap items-center justify-center">
-    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-yellow-abs rounded-lg max-w-xs shadow-lg">
-      <div class="relative pt-10 px-10 flex items-center justify-center">
-        <div
-          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
-        />
-        <img
-          class="relative w-40"
-          src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
-          alt=""
-        >
-      </div>
-      <div class="relative text-white px-6 pb-6 mt-6">
-        <div class="flex justify-center">
-          <span class="block bg-white rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$36.00</span>
+  <div>
+    <div
+      class="p-24 flex flex-wrap items-center justify-center"
+    >
+      <div
+        class="flex-shrink-0 m-6 relative overflow-hidden bg-red-arm rounded-lg max-w-xs shadow-lg"
+        v-for="(skil, index) in skils.skilArm"
+        :key="index"
+      >
+        <div class="relative text-white px-6 pb-6 mt-6">
+          <div class="flex justify-center">
+            <span class="block bg-white rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">{{ skil }}</span>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-red-arm rounded-lg max-w-xs shadow-lg">
-      <div class="relative pt-10 px-10 flex items-center justify-center">
-        <div
-          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
-        />
-        <img
-          class="relative w-40"
-          src="https://user-images.githubusercontent.com/2805249/64069998-305de300-cc9a-11e9-8ae7-5a0fe00299f2.png"
-          alt=""
-        >
-      </div>
-      <div class="relative text-white px-6 pb-6 mt-6">
-        <div class="flex justify-center">
-          <span class="block bg-white rounded-full text-teal-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$45.00</span>
+      <div
+        class="flex-shrink-0 m-6 relative overflow-hidden bg-yellow-abs rounded-lg max-w-xs shadow-lg"
+        v-for="(skil, index) in skils.skilAbs"
+        :key="index"
+      >
+        <div class="relative text-white px-6 pb-6 mt-6">
+          <div class="flex justify-center">
+            <span class="block bg-white rounded-full text-teal-500 text-xs font-bold px-3 py-2 leading-none flex items-center">{{ skil }}</span>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-purple-foot rounded-lg max-w-xs shadow-lg">
-      <div class="relative pt-10 px-10 flex items-center justify-center">
-        <div
-          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
-        />
-        <img
-          class="relative w-40"
-          src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
-          alt=""
-        >
-      </div>
-      <div class="relative text-white px-6 pb-6 mt-6">
-        <div class="flex justify-center">
-          <span class="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$68.50</span>
+      <div
+        class="flex-shrink-0 m-6 relative overflow-hidden bg-purple-foot rounded-lg max-w-xs shadow-lg"
+        v-for="(skil, index) in skils.skilFoot"
+        :key="index"
+      >
+        <div class="relative text-white px-6 pb-6 mt-6">
+          <div class="flex justify-center">
+            <span class="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">{{ skil }}</span>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-green-yoga rounded-lg max-w-xs shadow-lg">
-      <div class="relative pt-10 px-10 flex items-center justify-center">
-        <div
-          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
-        />
-        <img
-          class="relative w-40"
-          src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
-          alt=""
-        >
-      </div>
-      <div class="relative text-white px-6 pb-6 mt-6">
-        <div class="flex justify-center">
-          <span class="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$68.50</span>
-        </div>
-      </div>
-    </div>
-    <div class="flex-shrink-0 m-6 relative overflow-hidden bg-pink-heel rounded-lg max-w-xs shadow-lg">
-      <div class="relative pt-10 px-10 flex items-center justify-center">
-        <div
-          class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-          style="background: radial-gradient(black, transparent 60%); opacity: .2; transform: rotate3d(0, 0, 1, 20deg) scale3d(1, .6, 1);"
-        />
-        <img
-          class="relative w-40"
-          src="https://user-images.githubusercontent.com/2805249/64069899-8bdaa180-cc97-11e9-9b19-1a9e1a254c18.png"
-          alt=""
-        >
-      </div>
-      <div class="relative text-white px-6 pb-6 mt-6">
-        <div class="flex justify-center">
-          <span class="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$68.50</span>
+      <div
+        class="flex-shrink-0 m-6 relative overflow-hidden bg-green-yoga rounded-lg max-w-xs shadow-lg"
+        v-for="(skil, index) in skils.skilYoga"
+        :key="index"
+      >
+        <div class="relative text-white px-6 pb-6 mt-6">
+          <div class="flex justify-center">
+            <span class="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">{{ skil }}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -94,11 +52,47 @@
 </template>
 
 <script>
-export default {
-  mounted () {
-    console.log('Component mounted.')
+import { createComponent, reactive, computed, watch, onMounted } from '@vue/composition-api'
+import data from '../data/index.json'
+import axios from 'axios'
+export default createComponent({
+  setup () {
+    const state = reactive({
+      greet: 'hello',
+      skilArm: data[0].skil.arm
+    })
+
+    watch(() => {
+
+    })
+
+    computed(() => {
+
+    })
+    const skils = computed(() => {
+      const skilArm = data[0].skil.arm
+      const skilFoot = data[0].skil.foot
+      const skilAbs = data[0].skil.abs
+      const skilYoga = data[0].skil.yoga
+      return (
+        { skilArm, skilFoot, skilAbs, skilYoga }
+      )
+    })
+    async function getdata () {
+      const data = await axios.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.MIX_TMDB_KEY}&language=ja-JA`)
+      const results = data.data.results
+      return results
+    }
+    onMounted(() => {
+      getdata()
+    })
+
+    return {
+      state,
+      skils
+    }
   }
-}
+})
 </script>
 
 <style scoped>
